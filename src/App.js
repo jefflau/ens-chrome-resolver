8baa2bda1cbd51ae80d90ffb09881c52c1dc5a01
Jeff Lau <jeff-lau@live.com>
2018-08-11 13:08:20 +0100

    Add input box and search button linked with resolving

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import { providers } from 'ethers';
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { providers } from 'ethers'
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      ethers: new providers.InfuraProvider(providers.networks.mainnet)
-    }
-    this.state.ethers.resolveName('brettsun.eth').then(console.log)
+  state = {
+    ethers: {}
   }
+  componentDidMount() {
+    this.setState({
+      ethers: new providers.InfuraProvider(providers.networks.mainnet)
+    })
+  }
+  resolve = () => {
+    this.state.ethers.resolveName(this.input.value).then(address => {
+      this.setState({
+        address
+      })
+    })
+  }
+  // handleChange = e => {
+  //   console.log(e)
+  //   this.setState({
+  //     input: e.value
+  //   })
+  // }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" ref={input => (this.input = input)} />
+        <button onClick={() => this.resolve()}>Search</button>
+        <div>{this.state.address}</div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
